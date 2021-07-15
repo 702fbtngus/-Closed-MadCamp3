193c696467c5943acdfb8ba0ad5f6494163cd110
limetree06 <limetree06@naver.com>
2021-07-15 15:03:01 +0900

    first commit

---
+++ Assets/PlayerMove.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+    public float jump = 10f;
+    public float jump2 = 12f;
+
+    int jumpCount = 0;
+    public void Jump_Btn(){
+        if(jumpCount == 0){
+            gameObject.GetComponent<Rigidbody2D>().velocity = new Vector3(0, jump,0);
+            jumpCount +=1;
+        }
+
+
+
+    }
+
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
